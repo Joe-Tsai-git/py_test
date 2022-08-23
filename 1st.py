9bb20e0ff0b966c9aff5927723555bcec03fe059
joe-tsai-git <saga@163.com>
2022-08-23 12:59:56 +0800

    first commit by cq

---
+++ 1st.py
@@ -0,0 +1,9 @@
+print("Hello world")
+print("asdfasdfadsfa")
+import prettytable as pt
+
+tb = pt.PrettyTable()
+tb.field_names = ['Author', 'wwcheng']
+tb.add_row(['ID', 'xiaolvshijie'])
+
+print(tb)
