e59f70991c788ba56e3985526c1e2abba1a7393c
joe-tsai-git <saga@163.com>
2022-08-26 09:54:29 +0800

    ENV

--- 1st.py
+++ 1st.py
@@ -1,7 +1,6 @@
 print("Remote success! Hello your ABiii world")
 print("asdfYsdfadsfa")
 import prettytable as pt
-
 tb = pt.PrettyTable()
 tb.field_names = ['Author', 'wwcheng']
 tb.add_row(['ID', 'xiaolvshijie'])
