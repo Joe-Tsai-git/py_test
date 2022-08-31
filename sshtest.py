4fe9de6e575fe5e0910283ed541e53e8f2a270eb
joe-tsai-git <saga@163.com>
2022-08-31 17:14:44 +0800

    restore

--- sshtest.py
+++ sshtest.py
@@ -4,8 +4,7 @@ ssh = paramiko.SSHClient()
 
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # 允许连接不在know_hosts文件中的主机
 
-private_key = paramiko.RSAKey.from_private_key_file("../.ssh/id_rsa")
-ssh.connect("192.168.8.1",2222,"admin", pkey=private_key)
+ssh.connect("192.168.8.1",2222,"admin", "asuaadmin")
 
 execmd = 'pwd' #需要输入的命令
 
