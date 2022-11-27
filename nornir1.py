adf6aa2be99257b9a8f0f1910113034ae4e3c099
Joe-Tsai-git <saga@163.com>
2022-11-27 21:44:00 +0800

    2

--- nornir1.py
+++ nornir1.py
@@ -3,6 +3,6 @@ from nornir_napalm.plugins.tasks import napalm_get
 from nornir_utils.plugins.functions import print_result
 
 nr = InitNornir(config_file="config.yaml", dry_run=True)
-results = nr.run(task=napalm_get, getters=["facts","interfaces"])
+results = nr.run(task=napalm_get, getters=["facts","interfaces_ip"])
 
 print_result(results)
